92b63a66152d6766acb1cd07583b38e226ea2a93
RamonBecker <ramonbecker68@gmail.com>
2021-06-22 11:04:47 -0300

    Criando APIViews para o metodo HTTP GET

--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-
+import re
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoApiView(APIView):
+    """
+    Api de Cursos
+    """    
+    
+    def get(self, request):
+        print(request.user) 
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoApiView(APIView):
+    """
+    Api de Avaliação
+    """
+    
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+    
+    
