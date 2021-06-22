729efcca91cd446a9a11b6dd715eacbae3f7a62d
RamonBecker <ramonbecker68@gmail.com>
2021-06-22 11:33:27 -0300

    Criando APIViews para o metodo HTTP POST

--- cursos/views.py
+++ cursos/views.py
@@ -2,7 +2,7 @@ import re
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -18,7 +18,15 @@ class CursoApiView(APIView):
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
-
+    
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True) #verificando se os dados enviado são válidos, caso contrário manda uma exceção
+        serializer.save()
+        #return Response({"msg": "Criou com sucesso"}, status.HTTP_201_CREATED)
+        #return Response({"id": serializer.data['id'], 
+                         #"curso": serializer.data['titulo']}, status.HTTP_201_CREATED)
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 class AvaliacaoApiView(APIView):
     """
@@ -30,4 +38,8 @@ class AvaliacaoApiView(APIView):
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
     
-    
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
