5d51a7c8f71694b415c00745aefc8b1c05a8f5f2
RamonBecker <ramonbecker68@gmail.com>
2021-06-21 23:00:58 -0300

    Criando os primeiros models

--- cursos/models.py
+++ cursos/models.py
@@ -1,2 +1,42 @@
+from enum import unique
 from django.db import models
+from django.db.models import base
+from django.utils import tree
 
+class Base(models.Model):
+    criacao = models.DateField(auto_now=True)
+    atualizacao = models.DateField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Curso(Base):
+    titulo = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+    
+    def __str__(self):
+        return self.titulo
+
+class Avaliacao(Base):
+    curso = models.ForeignKey(Curso, related_name='avaliacoes', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    comentario = models.TextField(blank=True, default='')
+    nota = models.DecimalField(max_digits=2, decimal_places=1)
+    
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+        unique_together = ['email', 'curso'] # Cada pessoa identificada pelo e-mail só poderá avaliar uma vez o curso
+    
+    def __str__(self):
+        return f'{self.nome} avaliou o curso {self.curso} com nota {self.nota}' 
+        
+
+    
